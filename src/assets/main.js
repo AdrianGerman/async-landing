e4a09cde4cdb8d5fa3f3229c37bb6f9c146dbf7b
AdrianGerman <adrian.german4485@alumnos.udg.mx>
2023-10-11 09:52:24 -0600

    design and spanish languaje

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,11 +2,12 @@ const API =
   "https://youtube-v31.p.rapidapi.com/search?channelId=UCJUYcEdvnYFGajHBW0Nao3w&part=snippet%2Cid&order=date&maxResults=10";
 
 const content = null || document.getElementById("content");
+
 const options = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "8a51869689msh9d7845173c69b9ep155cb0jsn4a11a0272091",
     "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+    "X-RapidAPI-Key": "c68edbdba9mshc3cb6eb8038f3b2p1f444ajsna5de19fe1256",
   },
 };
 
@@ -16,32 +17,32 @@ async function fetchData(urlApi) {
   return data;
 }
 
-async () => {
+(async () => {
   try {
     const videos = await fetchData(API);
     let view = `
-    ${videos.item
+    ${videos.items
       .map(
         (video) => `
-    <div class="group relative">
-    <div
-        class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-        <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
-    </div>
-    <div class="mt-4 flex justify-between">
-        <h3 class="text-sm text-gray-700">
-            <span aria-hidden="true"
-                class="absolute inset-0"></span>
+      <div class="group relative">
+        <div
+          class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+          <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+        </div>
+        <div class="mt-4 flex justify-between">
+          <h3 class="text-sm text-white">
+            <span aria-hidden="true" class="absolute inset-0"></span>
             ${video.snippet.title}
-        </h3>
-    </div>
-    </div>
+          </h3>
+        </div>
+      </div>
     `
       )
-      .slice(0, 4)
-      .join("")} `;
+      .slice(0, 8)
+      .join("")}
+    `;
     content.innerHTML = view;
   } catch (error) {
     console.log(error);
   }
-};
+})();
